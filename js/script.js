aecd2d7cd3b6a75f2a2394d186ca634e31bb71b6
Nazar <kotsuyba@gmail.com>
2017-03-04 14:35:20 +0100

    Dodac strukturę i css wlacznie do section car

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/*$(window).scroll(function() {
+    var wScroll = $(this).scrollTop();
+    $('.iphone').css({
+        'transform': 'translate(0px,-' + wScroll / 20 + '%)'
+    });
+    $('.landscape figure ').css({
+        'transform': 'translate(0px,' + wScroll / 3.0 + '%)'
+    });
+});
+$(window).scroll(function() {
+
+  if ($(this).matchMedia('(max-width: 768px)').matches) {
+
+              var wScroll = $(this).scrollTop();
+              $('.iphone').css({
+                  'transform': 'translate(0px)'
+              });
+              $('.landscape figure ').css({
+                  'transform': 'translate(0px)'
+              });
+
+
+      }
+});*/
+
+// $(function checkPosition() {
+//     if (window.matchMedia('(min-width: 767px)').matches) {
+//         $(this).scroll(function() {
+//             var wScroll = $(this).scrollTop();
+//             $('#iphone').css({
+//                 'transform': 'translate(0px,-' + wScroll / 20 + '%)'
+//             });
+//             $('#landscape figure ').css({
+//                 'transform': 'translate(0px,' + wScroll / 3.0 + '%)'
+//             });
+//         });
+//     }
+// });
