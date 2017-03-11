28f32db9e5023226d4d6875d033a5ff1eafb153a
Nazar <kotsuyba@gmail.com>
2017-03-11 20:48:25 +0100

    ukonczyc standartna stlizacje

--- js/script.js
+++ js/script.js
@@ -1,42 +1,10 @@
-/*$(window).scroll(function() {
-    var wScroll = $(this).scrollTop();
-    $('.iphone').css({
-        'transform': 'translate(0px,-' + wScroll / 20 + '%)'
-    });
-    $('.landscape figure ').css({
-        'transform': 'translate(0px,' + wScroll / 3.0 + '%)'
-    });
-});
-$(window).scroll(function() {
-
-  if ($(this).matchMedia('(max-width: 768px)').matches) {
-
-              var wScroll = $(this).scrollTop();
-              $('.iphone').css({
-                  'transform': 'translate(0px)'
-              });
-              $('.landscape figure ').css({
-                  'transform': 'translate(0px)'
-              });
-
-
-      }
-});*/
-
-// $(function checkPosition() {
-//     if (window.matchMedia('(min-width: 767px)').matches) {
-//         $(this).scroll(function() {
-//             var wScroll = $(this).scrollTop();
-//             $('#iphone').css({
-//                 'transform': 'translate(0px,-' + wScroll / 20 + '%)'
-//             });
-//             $('#landscape figure ').css({
-//                 'transform': 'translate(0px,' + wScroll / 3.0 + '%)'
-//             });
-//         });
-//     }
-// });
-// $(function () {
+$(function () {
+   $('.boxes-wrapper .col').eq(0).addClass('block-1');
+   $('.boxes-wrapper .col').eq(1).addClass('block-2');
+   $('.boxes-wrapper .col').eq(2).addClass('block-3');
+   $('.boxes-wrapper .col').eq(3).addClass('block-4');
+   $('.boxes-wrapper .col').eq(4).addClass('block-5');
+   $('#boxes .container ').eq(0).addClass('grey');
   
 
-// })
+});
