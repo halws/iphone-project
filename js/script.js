5141bcb47addf3c60686b05ba43e65fb370e58a2
Nazar <kotsuyba@gmail.com>
2017-03-15 00:20:50 +0100

    troche kombinacji z przyciskami

--- js/script.js
+++ js/script.js
@@ -5,6 +5,39 @@ $(function () {
    $('.boxes-wrapper .col').eq(3).addClass('block-4');
    $('.boxes-wrapper .col').eq(4).addClass('block-5');
    $('#boxes .container ').eq(0).addClass('grey');
+   $('#buy-it .wrapper:first .inner:even , #buy-it .wrapper:last .inner:even').css({
+        'height':'40px',
+        // 'margin':'10px 0',
+        // 'border': '1px solid black'
+   })
+    
+   ;
   
 
 });
+$(function checkPosition() {
+        if (window.matchMedia('(min-width: 767px)').matches) {
+            var $samochod = $('#samochod');
+            var $paralaxSpeed = $samochod.data('speed');
+            var $window = $(window);
+            var $posY = '100vh' - ($window.scrollTop() / $paralaxSpeed);
+            var $coords = '50% ' + $posY + 'px';
+            $(this).scroll(function() {
+                var wScroll = $(this).scrollTop();
+                $('#iphone').css({
+                    'transform': 'translate(0px,-' + wScroll / 20 + '%)'
+                });
+                $('#landscape figure ').css({
+                    'transform': 'translate(0px,' + wScroll / 3.0 + '%)'
+                });
+                $('#bg').css({
+                    'top': -(wScroll * 0.10) + 'px'
+                });
+
+                $($samochod).css({
+                    backgroundPosition: $coords
+                });
+            });
+
+        }
+    });
